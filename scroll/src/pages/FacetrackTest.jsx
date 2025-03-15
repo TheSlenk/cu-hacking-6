9ef1d3214c3e7d148157610ccab3d30d8551326f
Edwin <edwin.nguim@gmail.com>
2025-03-15 18:23:44 -0400

    feat(facetrack) Setup the pages for testing

---
+++ scroll/src/pages/FacetrackTest.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FacetrackTest = () => {
+
+    return (
+        <div>
+            <h1>Facetrack Test Component</h1>
+            <p>get the status of the head movement and place it here (to prove we can do it)</p>
+        </div>
+    );
+};
+
+export default FacetrackTest;
