355271dea3163f492c5c7698178550e7855b6513
ryushen-tan <ryushen@gmail.com>
2025-03-15 17:23:26 -0400

    Feat: build out homepage

--- scroll/src/pages/Home.jsx
+++ scroll/src/pages/Home.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import DecryptedText from '../components/DecryptedText';
+import StarryBackground from '../components/StarryBackground';
 
 function Home() {
   return (
-    <div className="flex flex-col h-screen w-screen items-center justify-center bg-gray-100">
-      {/* TODO: bring the gradient back later */}
-      {/* text-transparent bg-clip-text text-gradient-to-r from-blue-500 to-purple-500 */}
-      <h1 className="text-6xl font-bold mb-6 text-blue-500">
-        Scroll.Ai
-      </h1>
-      <h2 className="text-xl font-bold mb-8 text-gray-700">
-        Focus on learning, we'll keep you focused.
-      </h2>
-      <input
-        type="text"
-        placeholder="I would like to learn about..."
-        className="w-1/2 p-4 border border-gray-300 rounded-full focus:outline-none mb-8"
-      />
-      <div className="flex space-x-4">
-        <Link to="/content" className="px-6 py-2 bg-blue-500 text-white rounded-full">
-          Search
-        </Link>
+    <div className="flex flex-col h-screen w-screen items-center justify-center relative">
+      <StarryBackground />
+      <div className="flex flex-col items-center justify-center overflow-hidden">
+        <div className="gap-12 w-[800px] fade-in h-[450px] bg-black/10 bg-blur-xl rounded-[65px] border-1 border-gray-400 backdrop-blur-xl flex flex-col justify-center items-center">
+          <div className="px-32 text-center z-10">
+            <DecryptedText
+              className="text-4xl font-bold text-white"
+              encryptedClassName="text-4xl font-bold text-white"
+              text="Welcome to Scroll... your new AI learning platform"
+              animateOn="view"
+              revealDirection="center"
+            />
+          </div>
+          <input
+            type="text"
+            placeholder="I would like to learn about..."
+            className="mt-3 w-[550px] p-4 border border-2 border-gray-300 text-white rounded-xl focus:outline-none"
+          />
+          <div className="flex space-x-4">
+            <Link to="/content" className="px-6 py-2 bg-white rounded-lg">
+              Search
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </div> 
   );
 }
 
