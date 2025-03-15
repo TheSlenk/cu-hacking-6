3cf93af14ddeed7ea382fd2d73dbd8e4bb3e0730
Edwin <edwin.nguim@gmail.com>
2025-03-15 02:11:02 -0400

    setup

---
+++ scroll/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>
+          search bar goes here
+        </p>
+        <h1>
+          content goes here
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
