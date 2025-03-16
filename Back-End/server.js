334c8da269c71833e1fc81833bb100059f733caf
SlenK <stephenmamosian@gmail.com>
2025-03-16 05:21:52 -0400

    Working image and script generation

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const {generateImage, generateScript} = require('./ai')
+const {generateImage, generateScript} = require('./ai.js')
 const PORT = 8080
 
 //#region GET
@@ -11,10 +11,47 @@ app.get('/', (req, res) => {
 app.get('/home', homePageHandler)
 //#endregion
 
+console.log(`Listening on port ${PORT}...`)
 app.listen(PORT)
 
 //#region Handler Functions
-function homePageHandler() {
-    res.status(200).send('Home Page!')
+async function homePageHandler(req, res) {
+    const result = await generateJsonScript('Math')
+    res.status(200).send(result)
+}
+//#endregion
+
+//#region Helper Functions
+async function generateJsonScript(prompt) {
+    const jsonScript = await generateScript(prompt)
+    const parsedJsonScript = parseJsonScript(jsonScript)
+    const sections = parsedJsonScript[Object.keys(parsedJsonScript).pop()]
+    const newSections = []
+    for (let section of sections) {
+        const img = section['image']
+        if (img) {
+            try {
+                const ans = await generateImage(img);
+                console.log(`Generated an image for "${img}"`)
+                section['image'] = ans
+                newSections.push(JSON.parse(JSON.stringify(section)))
+                continue
+            }
+            catch(err) {
+                console.log(`Unable to fetch images for "${img}"`);
+            }
+        }
+        section['image'] = undefined
+        newSections.push(JSON.parse(JSON.stringify(section)))
+    }
+
+    console.log(newSections)
+    return newSections
+}
+
+function parseJsonScript(jsonScript) {
+    jsonScript = jsonScript.replaceAll('```json', '')
+    jsonScript = jsonScript.replaceAll('```', '')
+    return JSON.parse(jsonScript)
 }
 //#endregion
